23881dfaaddbffd921572fa740252d6b33bc7d3f
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-17 12:15:38 +0200

    lection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './sass/main.scss';
+
+const baseUrl = 'http://localhost:3000';
+
+function reqServer(url) {
+  return fetch(baseUrl + url).then(res => res.json());
+}
+reqServer('/posts').then(data => console.log(data));
+
+const refs = {
+  listNode: document.querySelector('.post-list'),
+};
+console.log(refs.listNode);
+function renderPostList() {
+  reqServer('/posts').then(data => {
+    const markup = data
+      .map(
+        post => `<li data-id="${post.id}">
+        <h3>${post.title}</h3>
+        <p>${post.text}</p>
+        <small>${post.author}</small></p></li>`,
+      )
+      .join('');
+    refs.listNode.innerHTML = markup;
+  });
+}
+
+renderPostList();
